59c555d3c056915369a9a96fe8df48b606dfb81c
Monika Kot <kot.monika.0318@wp.pl>
2016-12-13 18:09:49 +0100

    Dodanie pierwszego pliku do repozytorium

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="utf-8"/>
+    <title> Moje słodkie hobby</title>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+<body>
+    <div id=pojemnik>
+        <h1>cudenka</h1>
+    </div>
+</body>
+
+</html>
